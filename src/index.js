e34c2d0c0f9ab44dbc9c0e56aaba6b517fe9ad1c
Mahim-Github <139007842+Mahim-Github@users.noreply.github.com>
2025-01-19 22:12:29 +0530

    Doing Extra

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // const div= React.createElement('div', {className: 'text'}, " Hello My name is Mahim !")
 // that's how we can make child
-const child1= React.createElement('p', {} , "My name is Mahim's Child")
-const child2= React.createElement('p',{}, "My name is Mahim's Second" )
+// const child1= React.createElement('p', {} , "My name is Mahim's Child")
+// const child2= React.createElement('p',{}, "My name is Mahim's Second" )
 //we can also create siblings like same and we can pass them in an array
 // const div= React.createElement('div', {className: 'text'}, [child1, child2])
 //after proper confiog we can use jsx syntax
 // like const div= <div className="text">Hello world</div> properly gonna work
-const div=<div className="text">Hello Mahim</div> 
+const apiData= "Hi It's currently afternoon";
+ 
+const isMorning=false;
+const morningtext= "Good Morning Mahim";
+const afternoonText="Good Afternoon Akshat";
+//const div=<div className="text">{isMorning ? morningtext : afternoonText}</div>
+const morningElement=<div>{morningtext} it is 8am</div>
+const afternoonElement= <div>{afternoonText} it is 10pm</div>
+const div= isMorning?morningElement:afternoonElement;
 console.log(div)
 const root= ReactDOM.createRoot(document.getElementById('root'))
 root.render(div)
