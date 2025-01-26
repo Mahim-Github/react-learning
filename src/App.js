33af826e565a2a146cf7666c7e07b579f2465ef2
Mahim-Github <139007842+Mahim-Github@users.noreply.github.com>
2025-01-26 11:13:25 +0530

    Now Switch to other channel

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 import Button from './Button.js'
 
 const App = () => {
-    return <Button>Click Me New</Button>
+    const clickAction=()=>{
+        console.log("I was Clicked from the parent")
+    }
+    return <Button clickAction={clickAction}>Click Me New</Button>
 }
 // export {App}
 export default App;
--- src/Button.js
+++ src/Button.js
@@ -1,9 +1,10 @@
 import React from 'react';
 const Button = (props) =>{
      console.log(props)
-     const {children}= props
+     const {children, clickAction}= props//const {children}= props
     const handleClick= () =>{
-        console.log ("Button is clicked inside the component")
+        // console.log ("Button is clicked inside the component")
+        clickAction()
     }
     return <button onClick={(handleClick)}>{children}</button>
 }
